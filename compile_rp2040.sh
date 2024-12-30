877ff2617c5d65dea5b42a7d47861c1183199d7d
parker <parker.g.britt@gmail.com>
2024-12-30 15:08:31 +0000

    auto mount rp2040 in compile script

--- compile_rp2040.sh
+++ compile_rp2040.sh
@@ -2,5 +2,16 @@
 
 qmk compile -kb crkbd -km parker -e CONVERT_TO=rp2040_ce && \
 echo "FINISHED COMPILING, FLASH NOW" && \
-sleep 10 && \
+sleep 1 && \
+echo "."
+sleep 1 && \
+echo "."
+sleep 1 && \
+echo "."
+sleep 1 && \
+echo "."
+sleep 1 && \
+echo "."
+sleep 1 && \
+udisksctl mount --block-device /dev/disk/by-label/RPI-RP2 && \
 cp crkbd_rev1_parker_rp2040_ce.uf2 /run/media/parker/RPI-RP2/
